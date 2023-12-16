14ebc5931b244dd9c866b99c52e52d0ffb49ef6a
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2023-12-16 21:39:32 +0545

    chore: remove all the testing files and began the fresh implementation of the real one

--- app/(test)/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TestPage = () => {
-  return (
-    <div className=''>TestPage</div>
-  )
-}
-
-export default TestPage
--- app/user/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserPage = ({ params }: { params: { id: string } }) => {
-  return <div>{`User id: ${params.id}`}</div>;
-};
-
-export default UserPage;
