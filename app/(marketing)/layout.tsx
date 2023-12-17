82fcd03b4af98dfc11fdebf9b7fab20e2bc19a19
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2023-12-17 22:02:03 +0545

    chore: add marketing page

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="h-full bg-slate-100">{children}</div>;
+};
+
+export default MarketingLayout;
