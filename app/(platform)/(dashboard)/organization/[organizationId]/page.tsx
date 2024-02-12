031c99cb5fca575e37dc4ffd64adcb1c726c2b6c
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2024-02-12 23:11:38 +0545

    chore: add a board for page in organization id

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,17 +1,13 @@
 import { db } from '@/lib/db'
 import { Board } from './board'
 import { Form } from './form'
+import { Info } from './components/info'
+import { useOrganization } from '@clerk/nextjs'
 
 const OrganizationPage = async () => {
-  const boards = await db.board.findMany()
   return (
-    <div className="flex flex-col space-y-2">
-      <Form />
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <Board key={board.id} title={board.title} id={board.id} />
-        ))}
-      </div>
+    <div className="w-full mb-20">
+      <Info />
     </div>
   )
 }
