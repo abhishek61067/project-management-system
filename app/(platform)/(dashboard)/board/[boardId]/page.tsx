d472889d40416b37f3d915daa47a5cd139887d89
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2024-05-07 21:02:06 +0545

    chore: make list

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,5 +1,45 @@
-const BoardIdPage = () => {
-  return <div className="mt-[100px]">Boardids</div>
+import { auth } from '@clerk/nextjs'
+import { db } from '@/lib/db'
+import { redirect } from 'next/navigation'
+import ListContainer from './components/ListContainer'
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string
+  }
+}
+
+const BoardIdPage = async ({ params }: BoardIdPageProps) => {
+  const { orgId } = auth()
+
+  if (!orgId) {
+    redirect('/select-org')
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: 'asc',
+        },
+      },
+    },
+    orderBy: {
+      order: 'asc',
+    },
+  })
+
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer boardId={params.boardId} data={lists} />
+    </div>
+  )
 }
 
 export default BoardIdPage
